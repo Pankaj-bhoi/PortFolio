4bb43c0b90986bdb83f76c4c525e69516236a60a
Pankaj <pankajcool.bhoi@gmail.com>
2019-03-20 21:27:59 +0530

    Updates

---
+++ precache-manifest.45c3e8114f1e2ea7ca7a4764ffc84677.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a2d3a28874746fdffad76d9c4d2a9eb",
+    "url": "/Portfolio/static/media/contact.1a2d3a28.png"
+  },
+  {
+    "revision": "9f7a125d0140b6d6e32c",
+    "url": "/Portfolio/static/css/main.50984f0a.chunk.css"
+  },
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.591eb5be.js"
+  },
+  {
+    "revision": "d178c738fd87526073d4",
+    "url": "/Portfolio/static/js/2.d178c738.chunk.js"
+  },
+  {
+    "revision": "08ee609e5cb400e940992b95f557f1ac",
+    "url": "/Portfolio/static/media/Wallpaper.08ee609e.png"
+  },
+  {
+    "revision": "a9fdcf2e71a5d8a7a957295b7aa86f62",
+    "url": "/Portfolio/static/media/about.a9fdcf2e.png"
+  },
+  {
+    "revision": "9f7a125d0140b6d6e32c",
+    "url": "/Portfolio/static/js/main.9f7a125d.chunk.js"
+  },
+  {
+    "revision": "4aed227362d8b0e77dfb02f368415ff3",
+    "url": "/Portfolio/static/media/pankaj.4aed2273.jpg"
+  },
+  {
+    "revision": "a5c2f9fbda25a65a8d95e2d80ee76a03",
+    "url": "/Portfolio/static/media/responsive.a5c2f9fb.png"
+  },
+  {
+    "revision": "8c4587d3a9361ce313107b92dd66fcc9",
+    "url": "/Portfolio/static/media/api.8c4587d3.png"
+  },
+  {
+    "revision": "13e69ce780cffabf77bea377994a3950",
+    "url": "/Portfolio/static/media/smoke.13e69ce7.png"
+  },
+  {
+    "revision": "57e4c98076d48a25b4752aa8a6cea5b7",
+    "url": "/Portfolio/index.html"
+  }
+];
